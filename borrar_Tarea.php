0d50c4451837d81859207db8d493f34bd89b6575
madeinbaires <52429399+madeinbaires@users.noreply.github.com>
2020-01-20 21:11:11 -0300

    Create borrar_Tarea.php

---
+++ borrar_Tarea.php
@@ -0,0 +1,25 @@
+<?php
+$coneccion= mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "desafio_crud"
+    );
+
+    if (isset ($_GET['id'])){
+        $id=$_GET['id'];
+        $query="DELETE from tareas where id=$id";
+        $resultado=mysqli_query($coneccion,$query);
+        if(!resultado){
+            die('Consulta fallida');        
+    }
+
+
+        $_SESSION['mensaje']="tarea removida correctamente";
+        $_SESSION['mensaje_tipo']="danger";
+        header("Location: index.php")
+        ;  
+    }
+  
+
+?>
